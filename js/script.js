6d911e7a8b016ae5e3e6235e65dbecda3b0e0542
Andrea <quadranteandrea@gmail.com>
2020-10-21 17:46:21 +0200

    Debugging

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 var ticketPricePerKm = 0.21;
-
-var userKm = prompt('Quanti chilometri vuoi percorrere?');
-var userAge = prompt('Quanti anni hai?');
-
 var ticketPrice = userKm * ticketPricePerKm + '€';
+var under18Discount = 0.2;
+var over65Discount = 0.4;
 
-console.log(ticketPrice)
+var userKm = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+var userAge = parseInt(prompt('Quanti anni hai?'));
+
+// console.log(ticketPrice);
+
+if (!isNaN(userKm) || !isNaN(userAge)) {
+  if (userAge < 18) {
+    ticketPrice - (ticketPrice * under18Discount);
+  } else if (userAge >= 65) {
+    ticketPrice - (ticketPrice * over65Discount);
+  } else {
+    ticketPrice;
+  }
+} else {
+    alert('Attenzione! Inserisci soltanto valori numerici.');
+}
+
+document.getElementById('ticket').innerHTML = 'Il tuo biglietto ha un prezzo di ' + ticketPrice;
