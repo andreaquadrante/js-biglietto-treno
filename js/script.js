49cd2bf07e2966b104e149b5b8d11ace377a2981
Andrea <quadranteandrea@gmail.com>
2020-10-22 16:10:14 +0200

    var Elements added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ var PRICE_PER_KM = 0.21;
 var UNDER_18_DISCOUNT = 0.2;
 var OVER_65_DISCOUNT = 0.4;
 
-var ticketNameEl = document.getElementById('ticketName')
-var ticketOfferEl = document.getElementById('ticketOffer')
-var ticketWagonEl = document.getElementById('ticketWagon')
-var ticketCodeEl = document.getElementById('ticketCode')
-var ticketPriceEl = document.getElementById('ticketPrice')
+var userNameEl = document.getElementById('userName');
+var userKmEl = document.getElementById('userKm');
+var userAgeEl = document.getElementById('userAge');
+
+var ticketNameEl = document.getElementById('ticketName');
+var ticketOfferEl = document.getElementById('ticketOffer');
+var ticketWagonEl = document.getElementById('ticketWagon');
+var ticketCodeEl = document.getElementById('ticketCode');
+var ticketPriceEl = document.getElementById('ticketPrice');
